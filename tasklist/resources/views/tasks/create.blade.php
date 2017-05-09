692d2b72179834ec01dfdb2a785ede413f8b96fd
ao <ao72aku@gmail.com>
2017-05-09 06:36:03 +0000

    kadai-tasklist

--- tasklist/resources/views/tasks/create.blade.php
+++ tasklist/resources/views/tasks/create.blade.php
@@ -2,20 +2,28 @@
 
 @section('content')
 
-    <h1>メッセージ新規作成ページ</h1>
+   
     
-    {!! Form::model($task,['route' => 'tasks.store']) !!}
-        
+<div class="row">
+    <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+     <h1>メッセージ新規作成ページ</h1>
+     {!! Form::model($task,['route' => 'tasks.store']) !!}
+     
+      <div class="form-group">    
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
+        {!! Form::text('status',null,['class' => 'form-control']) !!}
+      </div>
+      
+      <div class="form-group"> 
         {!! Form::label('content','メッセージ:') !!}
-        {!! Form::text('content') !!}
-        
-        {!! Form::submit('投稿') !!}
+        {!! Form::text('content',null,['class' => 'form-control']) !!}
+      </div>
+      
+        {!! Form::submit('投稿',['class' => 'btn btn-primary']) !!}
     
     {!! Form::close() !!}
         
-    
+    </div>
+</div>
 
 @endsection
--- tasklist/resources/views/tasks/edit.blade.php
+++ tasklist/resources/views/tasks/edit.blade.php
@@ -2,17 +2,24 @@
 
 @section('content')
 
+   
+<div class="row">
+    <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">    
     <h1>id: {{ $task->id}}のメッセージ編集ページ</h1>
-    
     {!! Form::model($task,['route' => ['tasks.update',$task->id],'method' => 'put']) !!}
-        
+        <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status',null, ['class' => 'form-control']) !!}
+        </div>
         
+        <div class="form-group">
         {!! Form::label('content','メッセージ:') !!}
-        {!! Form::text('content') !!}
-        
+        {!! Form::text('content',null, ['class' => 'form-control']) !!}
+        </div>
+   
         {!! Form::submit('更新') !!}
+        </div>
+</div>
         
     {!! Form::close() !!}
 
